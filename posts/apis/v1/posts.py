63763c35f8c1bf9d88398c451e25f05c169fc59e
Ryzen <sosnovscky.nikita@yandex.ru>
2022-01-19 18:04:35 +0300

    working_fixes

--- posts/apis/v1/posts.py
+++ posts/apis/v1/posts.py
@@ -1,6 +1,6 @@
 from arrested import Resource
 from arrested.contrib.kim_arrested import KimEndpoint
-from arrested.contrib.sql_alchemy import DBListMixin, DBCreateMixin
+from arrested.contrib.sql_alchemy import DBListMixin, DBCreateMixin, DBObjectMixin
 
 from posts.models import db, Post
 from .mappers import PostMapper
@@ -21,4 +21,18 @@ class PostsIndexEndpoint(KimEndpoint, DBListMixin, DBCreateMixin):
         return stmt
 
 
+class PostsObjectEndpoint(KimEndpoint, DBObjectMixin):
+
+    name = 'object'
+    url = '/<string:obj_id>'
+    mapper_class = PostMapper
+    model = Post
+
+    def get_query(self):
+
+        stmt = db.session.query(Post)
+        return stmt
+
+
 posts_resource.add_endpoint(PostsIndexEndpoint)
+posts_resource.add_endpoint(PostsObjectEndpoint)
